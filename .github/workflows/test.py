36c0d13831707aa283d88fa39bf8e9db8abc9471
DK-Guilherme <guilhermegsouza520@gmail.com>
2024-05-27 22:57:49 -0300

    test: testing with another url

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -16,6 +16,6 @@ def check_api_authentication(url):
         return f"An error occurred: {e}"
 
 # Example usage
-api_url = 'https://localhost:3000/api/items'
+api_url = 'https://www.youtube.com/'
 result = check_api_authentication(api_url)
 print(result)
